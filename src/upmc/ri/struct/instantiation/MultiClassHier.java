6fbe415a8852b167a510528c2eb61639222f1a99
clemthenem <clementberti@yahoo.fr>
2019-01-17 18:37:13 +0100

    finalise TME8

--- src/upmc/ri/struct/instantiation/MultiClassHier.java
+++ src/upmc/ri/struct/instantiation/MultiClassHier.java
@@ -11,27 +11,44 @@ public class MultiClassHier extends MultiClass {
 	public MultiClassHier(){
 		super();
 		
-		this.distances = new double[this.dict.size()][this.dict.size()];
+		int n = this.dict.size();
+		/* Init to 0 */
+		this.distances = new double[n][n];
 		ILexicalDatabase bd = new NictWordNet();
 		RelatednessCalculator calculator = new WuPalmer(bd);
+
+		double min = Double.MAX_VALUE;
+		double max = - Double.MAX_VALUE;
+		
 		for(String class1: enumerateY()) {
 			int ŷ = this.dict.get(class1);
 			
 			for(String class2: enumerateY()) {
 				int y = this.dict.get(class2);
-				double similarity = calculator.calcRelatednessOfWords(class1, class2);
-				if (!class1.equals(class2)) {
-					this.distances[ŷ][y];
+				
+				if (ŷ < y) {
+					this.distances[ŷ][y] = 1 - calculator.calcRelatednessOfWords(class1, class2);
+					
+					if (this.distances[ŷ][y] < min)
+						min = this.distances[ŷ][y];
+					
+					if (this.distances[ŷ][y] > max)
+						max = this.distances[ŷ][y];
 				}
+			}
+		}
+		
+		/* Linear normalization and copy on "the other side" of the array */
+		for (int i=1; i < n-1; i++){
+			for (int j=i+1; j < n; j++){
+				this.distances[i][j] = 1.9 * (this.distances[i][j] - min) / (max - min) + .1;
+				this.distances[j][i] = this.distances[i][j];
 			}
 		}
 	}
 	
 	@Override
 	public double delta(String y1, String y2) {
-		if (y1.equals(y2)) {
-			return 0;
-		}
-		return 1;
+		return this.distances[this.dict.get(y1)][this.dict.get(y2)];
 	}
 }
